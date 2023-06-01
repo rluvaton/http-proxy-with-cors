7d825a7b959d178a2c72416355d104396ed9a504
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2023-06-01 23:13:16 +0300

    feat(file): allow getting the servers using `--file` flag

--- README.md
+++ README.md
@@ -3,14 +3,38 @@
 Proxy HTTP requests with cors enabled.
 
 ## Usage
+It will create a proxy server on each local port that will proxy all requests to the upstream server.
 
+### CLI arguments
 ```bash
 npx proxy-http-with-cors <local-port-1> <upstream-1> ... <local-port-n> <upstream-n>
 ```
 
-It will create a proxy server on each local port that will proxy all requests to the upstream server.
+### From file
+```bash
+npx proxy-http-with-cors --file <path-to-json-file>
+```
 
+Example config file:
+```json
+[
+  {
+    "port": 3000,
+    "upstream": "https://us1.api.server.com"
+  },
+  {
+    "port": 3001,
+    "upstream": "https://us2.api.server.com"
+  }
+]
+```
 
 ### Options
 
+#### Config
+you can pass the server from a config file using (`-f` or `--file`) or pass them as `port` and `upstream` arguments.
+
+#### Logging
 To disable logging, set the `LOG_DISABLED` environment variable to `false`.
+
+
--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,5 @@
+import fs from 'node:fs';
+
 import { isValidHttpUrl } from './utils.js';
 import { CLIError } from './error.js';
 
@@ -9,6 +11,9 @@ It will create a proxy server on each local port that will proxy all requests to
 
 Env vars:
   - LOG_DISABLED: setting it to 'false' will disable logging
+
+Options:
+  -f, --file | a file path to a JSON file containing the servers configuration in the format: {port: number, upstream: string}[]
 `);
 }
 
@@ -17,45 +22,108 @@ Env vars:
  * @return {{{port: number, upstream: string}[]}}
  */
 export function parseArgs(args) {
-  if (args.length % 2 !== 0) {
+  const fileIndex = args.findIndex((arg) => arg === '-f' || arg === '--file');
+  const useFile = fileIndex !== -1;
+
+  /**
+   * @type {{port: string | number, upstream: string}[]}
+   */
+  let config = [];
+
+  if (!useFile && args.length % 2 !== 0) {
     throw new CLIError({
       message: 'Invalid number of arguments',
       showHelp: true,
     });
   }
 
+  if (useFile) {
+    const configPath = args[fileIndex + 1];
+
+    if (!configPath) {
+      throw new CLIError({
+        message: 'Missing file path',
+        showHelp: true,
+      });
+    }
+
+    let configFromFile = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+
+    if (!Array.isArray(configFromFile)) {
+      configFromFile = [configFromFile];
+    }
+    config = configFromFile;
+  } else {
+    config = [];
+
+    for (let i = 0; i < args.length; i += 2) {
+      config.push({
+        port: args[i],
+        upstream: args[i + 1],
+      });
+    }
+  }
+
   const servers = [];
-  for (let i = 0; i < args.length; i += 2) {
-    const port = parseInt(args[i]);
-    const isValidPort = port >= 0 && port <= 65536;
+  for (const server of config) {
+    const port = assertPortIsValid(server.port);
+    const upstream = assertUpstreamIsValid(server.upstream);
 
-    if (!isValidPort) {
-      throw new CLIError({
-        message: `Invalid port number ${args[i]}`,
-        showHelp: false,
-      });
-    }
+    assertServerConfigIsValid(servers, { port, upstream });
 
-    const upstream = args[i + 1];
-    if (!isValidHttpUrl(upstream)) {
-      throw new CLIError({
-        message: `Invalid upstream ${upstream}, must start with http:// or https://`,
-        showHelp: false,
-      });
-    }
-
-    if (servers.find((server) => server.port === port)) {
-      throw new CLIError({
-        message: `Port ${port} is already in use by another upstream, please use a different port`,
-        showHelp: false,
-      });
-    }
-
-    servers.push({
-      port: parseInt(args[i]),
-      upstream: upstream,
-    });
+    servers.push({ port, upstream });
   }
 
   return servers;
+}
+
+/**
+ *
+ * @param {string | number} portArg
+ * @return {number}
+ */
+function assertPortIsValid(portArg) {
+  const port = parseInt(portArg);
+  const isValidPort = port >= 0 && port <= 65536;
+
+  if (!isValidPort) {
+    throw new CLIError({
+      message: `Invalid port number ${portArg}`,
+      showHelp: false,
+    });
+  }
+
+  return port;
+}
+
+/**
+ *
+ * @param {string} upstream
+ * @return {string}
+ */
+function assertUpstreamIsValid(upstream) {
+  if (!upstream) {
+    throw new CLIError({
+      message: `Missing upstream`,
+      showHelp: false,
+    });
+  }
+
+  if (!isValidHttpUrl(upstream)) {
+    throw new CLIError({
+      message: `Invalid upstream ${upstream}, must start with http:// or https://`,
+      showHelp: false,
+    });
+  }
+
+  return upstream;
+}
+
+function assertServerConfigIsValid(servers, singleServer) {
+  if (servers.find((server) => server.port === singleServer.port)) {
+    throw new CLIError({
+      message: `Port ${singleServer.port} is already in use by another upstream, please use a different port`,
+      showHelp: false,
+    });
+  }
 }
